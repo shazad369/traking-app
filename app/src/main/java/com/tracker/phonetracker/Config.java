5c0bb4492bbcec376fcd1f9e17c1d18978a3b59c
shazad369 <shazadahamed571@gmail.com>
2026-05-31 20:33:12 +0600

    Delete app directory

--- app/src/main/java/com/tracker/phonetracker/Config.java
+++
@@ -1,8 +0,0 @@
-package com.tracker.phonetracker;
-
-public class Config {
-    // ⚠️ এখানে তোমার Linux computer-এর IP দাও
-    public static final String SERVER_IP = "103.148.74.82";
-    public static final int SERVER_PORT = 8080;
-    public static final String SERVER_URL = "http://" + SERVER_IP + ":" + SERVER_PORT + "/track";
-}
