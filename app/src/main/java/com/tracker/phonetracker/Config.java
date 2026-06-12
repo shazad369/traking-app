6e022658827924de3b7de6fb86bb547298409138
shazad369 <shazadahamed571@gmail.com>
2026-06-13 00:18:35 +0600

    Update Config.java

--- app/src/main/java/com/tracker/phonetracker/Config.java
+++ app/src/main/java/com/tracker/phonetracker/Config.java
@@ -4,5 +4,5 @@ public class Config {
     // ⚠️ এখানে তোমার Linux computer-এর IP দাও
    public static final String SERVER_IP = "103.148.74.82";
     public static final int SERVER_PORT = 8080;
-public static final String SERVER_URL = "https://fe709731e53bdf7c-103-148-74-80.serveousercontent.com/track";
+public static final String SERVER_URL = "https://5b4ad24f791908a4-103-148-74-80.serveousercontent.com";
 }
